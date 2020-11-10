3ced26d37923fdeb6dfc5433743e8733a47cc37e
Steve Winton <stevewinton@gmail.com>
2020-11-10 16:58:02 -0600

    Generate download URL dynamically

--- dist/index.js
+++ dist/index.js
@@ -41,17 +41,28 @@ if (require.main === require.cache[eval('__filename')]) {
 
 const os = __webpack_require__(87);
 
-const core = __webpack_require__(186);
+// arch in [arm, x32, x64...] (https://nodejs.org/api/os.html#os_os_arch)
+// return value in [amd64, 386, arm]
+function mapArch(arch) {
+  const mappings = {
+    x32: '386',
+    x64: 'amd64'
+  };
+  return mappings[arch] || arch;
+}
+
+// os in [darwin, linux, win32...] (https://nodejs.org/api/os.html#os_os_platform)
+// return value in [darwin, linux, windows]
+function mapOS(os) {
+  const mappings = {
+    darwin: 'macOS',
+    win32: 'windows'
+  };
+  return mappings[os] || os;
+}
 
 function getDownloadURL(version) {
-  core.debug(`platform: ${ os.platform() }`);
-  core.debug(`arch: ${ os.arch() }`);
-  core.debug(version);
-
-  // https://github.com/cli/cli/releases/download/v1.1.0/gh_1.1.0_linux_amd64.tar.gz
-  // https://github.com/cli/cli/releases/download/v1.1.0/gh_1.1.0_macOS_amd64.tar.gz
-  // https://github.com/cli/cli/releases/download/v1.1.0/gh_1.1.0_windows_amd64.zip
-  return `https://github.com/cli/cli/releases/download/v${ version }/gh_${ version }_linux_amd64.tar.gz`
+  return `https://github.com/cli/cli/releases/download/v${ version }/gh_${ version }_${ mapOS(os.platform()) }_${ mapArch(os.arch()) }.tar.gz`
 }
 
 module.exports = { getDownloadURL }
