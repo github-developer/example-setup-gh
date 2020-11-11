676f04d14aaf48274b627633f3cacf748cbb7d27
Steve Winton <stevewinton@gmail.com>
2020-11-11 11:12:32 -0600

    Refactor to support Windows

--- dist/index.js
+++ dist/index.js
@@ -8,31 +8,32 @@ require('./sourcemap-register.js');module.exports =
 const path = __webpack_require__(622);
 const core = __webpack_require__(186);
 const tc = __webpack_require__(784);
-const { getDownloadURL } = __webpack_require__(918);
+const { getDownloadObject } = __webpack_require__(918);
 
 async function setup() {
-  // Get version of tool to be installed
-  const version = core.getInput('version');
+  try {
+    // Get version of tool to be installed
+    const version = core.getInput('version');
 
-  // Download the specific version of the tool, e.g. as a tarball
-  const downloadURL = getDownloadURL(version);
-  const pathToTarball = await tc.downloadTool(downloadURL.url);
+    // Download the specific version of the tool, e.g. as a tarball/zipball
+    const download = getDownloadObject(version);
+    const pathToTarball = await tc.downloadTool(download.url);
 
-  // Extract the tarball onto host runner
-  const pathToCLI = await tc.extractTar(pathToTarball);
+    // Extract the tarball/zipball onto host runner
+    const extract = download.url.endsWith('.zip') ? tc.extractZip : tc.extractTar;
+    const pathToCLI = await extract(pathToTarball);
 
-  // Expose the tool by adding it to the PATH
-  core.addPath(path.join(pathToCLI, downloadURL.filename, 'bin'));
+    // Expose the tool by adding it to the PATH
+    core.addPath(path.join(pathToCLI, download.binPath));
+  } catch (e) {
+    core.setFailed(e);
+  }
 }
 
 module.exports = setup
 
 if (require.main === require.cache[eval('__filename')]) {
-  try {
-    setup();
-  } catch (e) {
-    core.error(e);
-  }
+  setup();
 }
 
 
@@ -42,6 +43,7 @@ if (require.main === require.cache[eval('__filename')]) {
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 const os = __webpack_require__(87);
+const path = __webpack_require__(622);
 
 // arch in [arm, x32, x64...] (https://nodejs.org/api/os.html#os_os_arch)
 // return value in [amd64, 386, arm]
@@ -63,16 +65,19 @@ function mapOS(os) {
   return mappings[os] || os;
 }
 
-function getDownloadURL(version) {
-  const filename = `gh_${ version }_${ mapOS(os.platform()) }_${ mapArch(os.arch()) }`;
-  const url = `https://github.com/cli/cli/releases/download/v${ version }/${ filename }.tar.gz`;
+function getDownloadObject(version) {
+  const platform = os.platform();
+  const filename = `gh_${ version }_${ mapOS(platform) }_${ mapArch(os.arch()) }`;
+  const extension = platform === 'win32' ? 'zip' : 'tar.gz';
+  const binPath = platform === 'win32' ? 'bin' : path.join(filename, 'bin');
+  const url = `https://github.com/cli/cli/releases/download/v${ version }/${ filename }.${ extension }`;
   return {
     url,
-    filename
+    binPath
   };
 }
 
-module.exports = { getDownloadURL }
+module.exports = { getDownloadObject }
 
 
 /***/ }),
--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const path = require('path');
 const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
-const { getDownloadURL } = require('./lib/utils');
+const { getDownloadObject } = require('./lib/utils');
 
 async function setup() {
-  // Get version of tool to be installed
-  const version = core.getInput('version');
+  try {
+    // Get version of tool to be installed
+    const version = core.getInput('version');
 
-  // Download the specific version of the tool, e.g. as a tarball
-  const downloadURL = getDownloadURL(version);
-  const pathToTarball = await tc.downloadTool(downloadURL.url);
+    // Download the specific version of the tool, e.g. as a tarball/zipball
+    const download = getDownloadObject(version);
+    const pathToTarball = await tc.downloadTool(download.url);
 
-  // Extract the tarball onto host runner
-  const pathToCLI = await tc.extractTar(pathToTarball);
+    // Extract the tarball/zipball onto host runner
+    const extract = download.url.endsWith('.zip') ? tc.extractZip : tc.extractTar;
+    const pathToCLI = await extract(pathToTarball);
 
-  // Expose the tool by adding it to the PATH
-  core.addPath(path.join(pathToCLI, downloadURL.filename, 'bin'));
+    // Expose the tool by adding it to the PATH
+    core.addPath(path.join(pathToCLI, download.binPath));
+  } catch (e) {
+    core.setFailed(e);
+  }
 }
 
 module.exports = setup
 
 if (require.main === module) {
-  try {
-    setup();
-  } catch (e) {
-    core.error(e);
-  }
+  setup();
 }
